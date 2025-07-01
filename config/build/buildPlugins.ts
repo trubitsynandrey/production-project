e37642c6f72e32550fa865f380a92905e8bd7a45
iverstoon6 <iverstoon6@gmail.com>
2025-07-01 13:49:58 +0400

    fix: no bundle analyzer in prod build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,10 +17,6 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
 
   if (isDev) {
